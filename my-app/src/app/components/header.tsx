77da0ffca659ed7c7016af9270bc2f2a29261029
SyedaEman002/SyedaEman002 <emansyeda189@gmail.com>
2024-12-16 18:28:36 +0500

    first commit

---
+++ my-app/src/app/components/header.tsx
@@ -0,0 +1,162 @@
+const Header =()=>{
+    return(
+        
+        <div>
+            <header className="text-slate-50 bg-slate-950 text-[18px] text-center">Sign up and get 20% off to your first order.<u className="text-[16px] px-[12px]">Sign Up Now</u></header>
+            <div className="bg-slate-50">
+                <img src="/search.jpg" alt="search" width={11000} height={1}></img>
+            
+                </div>
+                <div className="bg-slate-50 p-[122px]">
+                <img src="/Header.jpg" alt="Header" width={10000} height={1}></img>
+                    <h1 className="text-slate-950 font-extrabold font-[Integral CF] text-[52px]">FIND CLOTHES<br />THAT MATHCHES<br />YOUR STYLE</h1>
+                    <p className="text-gray-600 text-[24px] font-thin">Browse throught our diverse range of meticously crafted garments,<br /> designed to bring out your individuality and cater to your sense of style.</p>
+                    <button className="text-slate-50 bg-slate-950 rounded-lg px-[64px] p-[10px] radius-[62px] m-[18px]">Shop Now</button></div>
+
+<div className="bg-slate-50 ">
+  <h1 className="text-slate-950 text-[42px] text-center font-extrabold">NEW ARRIVALS</h1>
+</div>
+                    <section className="bg-gray-200">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-5">
+             {/* Image 1 */}
+             <div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/first.jpg" alt="first"width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">T-SHIRT WITH TAPE DETAILS</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 4.5/5</div>
+          <p className="text-xl font-bold mt-2 text-slate-950">$120</p>
+              </div>
+            </div>
+
+ {/* Image 2 */}
+ <div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/second.jpg" alt="second" width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Skinny Fit Jeans</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 3.5/5</div>
+                <p className="text-slate-950 mt-2 font-bold">$240</p>
+              </div>
+            </div>
+{/* Image 3 */}
+<div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/third.jpg" alt="third" width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Checkered Shirt</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 4.5/5</div>
+                <p className="text-slate-950 mt-2 font-bold">$180</p>
+                </div>
+            </div>
+            {/* Image 4 */}
+            
+            <div className=" bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/fourth.jpg" alt="fourth"height={100} width={400} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Sleeve Striped T-shirt</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 4.5/5</div>
+                <p className="text-slate-950 mt-2 font-bold">$130</p>
+              </div>
+              </div>
+              </div></div></section>
+              <div className="bg-slate-50 px-[745px] p-[28px]">
+            <button className="text-slate-950 rounded-lg p-[18px] px-[42px] border: black outline">View All</button>
+                </div>
+                <div className="bg-slate-50">
+                  <h1 className="text-slate-950 font-extrabold text-center text-[42px]">TOP SELLING</h1>
+                </div>
+                
+                <section className="bg-gray-200">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-5">
+             {/* Image 1 */}
+             <div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/firstimage.jpg" alt="firstimage"width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Vertical Striped Shirt</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 5.0/5</div>
+          <p className="text-xl font-bold mt-2 text-slate-950">$212</p>
+              </div>
+            </div>
+
+             {/* Image 2 */}
+             <div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/secondimage.jpg" alt="secondimage"width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Courage Graphic T-shirt</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 4.0/5</div>
+          <p className="text-xl font-bold mt-2 text-slate-950">$145</p>
+              </div>
+            </div>
+            
+
+            
+             {/* Image 3 */}
+             <div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/thirdimage.jpg" alt="thirdimage"width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Loose Fit Bermuda Shorts</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 3.0/5</div>
+          <p className="text-xl font-bold mt-2 text-slate-950">$80</p>
+              </div>
+            </div>
+
+
+            {/* Image 4 */}
+            <div className="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
+              <img src="/fourtimage.jpg" alt="fourtimage"width={400} height={100} />
+              <div className="p-4">
+                <h1 className="text-xl font-bold text-slate-950">Faded Skinny Jeans</h1>
+                <div className="text-slate-950 mt-2">⭐⭐⭐⭐⭐ 4.5/5</div>
+          <p className="text-xl font-bold mt-2 text-slate-950">$120</p>
+              </div>
+            </div>
+            </div></div></section>
+            <div className="bg-slate-50 px-[745px] p-[28px]">
+            <button className="text-slate-950 rounded-lg p-[18px] px-[42px] border: black outline">View All</button>
+                </div>
+
+
+               < div className="flex flex-col items-center justify-center bg-gray-100">
+      <div className="text-[42px] font-extrabold  text-slate-950">BROWSE BY DRESS STYLE</div>
+
+      <div className="grid grid-cols-2 gap-4 w-[90%] max-w-4xl">
+        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="/casualimage.jpg" alt="casualimage" width={700} height={300} className="object-cover"></img>
+          <span className="absolute text-xl font-medium text-black">Casual</span>
+        </div>
+
+        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="/formalimage.jpg" alt="formalimage" width={900} height={300} className="object-cover"></img>
+          <span className="absolute text-xl font-medium text-black">Formal</span>
+        </div>
+
+        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="/partyimage.jpg" alt="partyimage" height={300} width={900}></img>
+          <span className="absolute text-xl font-medium text-black">Party</span>
+        </div>
+
+        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="/gymimage.jpg" alt="gymimage" width={900} height={300}></img>
+          <p className="text-xl font-medium text-black">Gym</p>
+        </div>
+      </div>
+    </div>
+    <div className="h-screen bg-slate-50">
+      <img src="/footerimage.jpg" alt="footerimage" width={1500} height={400}></img>
+    </div>
+                
+                
+                
+                
+                
+                
+                
+            
+               
+            
+          </div>      
+                
+        
+    )
+}
+export default Header;
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/header";
+import {Poppins} from "next/font/google";
+
+const Home =()=> {
+  return (
+    <Header />
+
+  );
+}
+export default Home;
